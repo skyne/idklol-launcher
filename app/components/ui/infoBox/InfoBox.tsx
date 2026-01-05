c422a6cd6825fdf9e26c31e9cbae401958eb3e1a
Daniel Heringei <daniel.heringei@incepteam.com>
2026-01-05 12:46:16 +0100

    make infobox hover effect optional

--- app/components/ui/infoBox/InfoBox.tsx
+++ app/components/ui/infoBox/InfoBox.tsx
@@ -6,7 +6,7 @@ type InfoBoxProps = {
 
 export const InfoBox = ({ icon, title, onClick }: InfoBoxProps) => {
   return (
-    <div className="flex gap-2 items-center p-2 hover:underline cursor-pointer" onClick={onClick}>
+    <div className={`flex gap-2 items-center p-2 ${onClick ? 'hover:underline cursor-pointer' : ''}`} onClick={onClick}>
       <div className="flex w-8 h-8">
         <img src={icon} alt="info_box_icon" />
       </div>
